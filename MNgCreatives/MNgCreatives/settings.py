dc68b506c6daff271ad9e17e3e86f73c615c2340
EmmanuelMwanik <emmanuelmwanik98@gmail.com>
2026-03-19 16:09:49 +0300

    static files set up

--- MNgCreatives/MNgCreatives/settings.py
+++ MNgCreatives/MNgCreatives/settings.py
@@ -1,5 +1,6 @@
 
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,3 +106,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
