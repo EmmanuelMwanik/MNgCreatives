0fd010e2a168d1e6d6b5eaee04c0f20bc30622e0
EmmanuelMwanik <emmanuelmwanik98@gmail.com>
2026-05-21 14:03:46 +0300

    Fix static storage settings syntax

--- MNgCreatives/MNgCreatives/settings.py
+++ MNgCreatives/MNgCreatives/settings.py
@@ -123,13 +123,6 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-    STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
-    }
+}
