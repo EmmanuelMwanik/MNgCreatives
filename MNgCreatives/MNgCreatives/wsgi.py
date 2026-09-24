bcd6869a518434e8c99036f2ed175bfbcbfa0d9b
EmmanuelMwanik <emmanuelmwanik98@gmail.com>
2026-09-24 23:43:36 +0300

    Fix Django WSGI import path

--- MNgCreatives/MNgCreatives/wsgi.py
+++ MNgCreatives/MNgCreatives/wsgi.py
@@ -1,3 +1,4 @@
+```python
 """
 WSGI config for MNgCreatives project.
 
@@ -8,9 +9,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Add the Django project directory to Python's import path
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if PROJECT_DIR not in sys.path:
+    sys.path.insert(0, PROJECT_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MNgCreatives.settings')
 
 application = get_wsgi_application()
+```
