88c9dac37abbb87e3df05d9dc0878e87553eade5
EmmanuelMwanik <emmanuelmwanik98@gmail.com>
2026-09-24 15:12:44 +0300

    Fix contact form submissions

--- MNgCreatives/MNgApp/views.py
+++ MNgCreatives/MNgApp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
+from .forms import ContactForm
 
 
 
@@ -21,7 +24,17 @@ def resume(request):
     return render(request, 'resume.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent. Thank you!')
+            return redirect('contact')
+        messages.error(request, 'Please correct the errors below and try again.')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
 
 def testimonials(request):
     return render(request, 'testimonials.html')
